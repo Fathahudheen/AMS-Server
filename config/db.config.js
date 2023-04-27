3da0564ff13dd7dd8c064e468c9b7f794dedda55
Fathahudheen <67535358+Fathahudheen@users.noreply.github.com>
2023-04-27 17:29:14 +0530

    addedd comment to config

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://127.0.0.1:27017/Licensee_Mngt'
+    url: 'mongodb://127.0.0.1:27017/Licensee_Mngt'  //Licensee_mngt => database name.Do not change this name
 }
