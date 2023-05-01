afaac9df55c3a18569ac8a35601e57dc1af01a7e
rithu <rithu7025@gmail.com>
2023-05-01 16:30:04 +0530

    sorting and uppercase

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dbConfig = require ('./config/db.config')
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:3000"
+    origin: "http://localhost:3001"
   };
 app.use(cors(corsOptions));
 
