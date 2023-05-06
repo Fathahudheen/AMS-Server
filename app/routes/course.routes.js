bc9418c25a15697a53fb2860b8db5f35c95c58db
zainmp <wwwzainzaan@gmail.com>
2023-05-06 11:01:35 +0530

    course master datas updated

--- app/routes/course.routes.js
+++ app/routes/course.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const CourseController = require("../controllers/course.cntrl");
+const CourseController = require("../controllers/course.cntrl ")
 const router = express.Router();
+
 
 router.get("/", CourseController.findAll);
 router.get("/:id", CourseController.findOne);
+router.post("/", CourseController.create);
+router.patch("/:id",CourseController.update);
+router.delete('/:id', CourseController.destroy);
 
 
 module.exports = router;
